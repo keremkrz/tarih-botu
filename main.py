19a145ff43d0282371c3d0390a81cc4860210bce
Kerem Kiraz <104232003+keremkrz@users.noreply.github.com>
2025-11-25 12:43:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ client = tweepy.Client(
     consumer_key=os.getenv("CONSUMER_KEY"),
     consumer_secret=os.getenv("CONSUMER_SECRET"),
     access_token=os.getenv("ACCESS_TOKEN"),
-    access_token=os.getenv("ACCESS_TOKEN_SECRET")
+    access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
 )
 
 # CSV dosyasını oku
